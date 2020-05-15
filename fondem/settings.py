d0daf627a77bebb6a56a2a4542c39e562a7b8a30
chrioniep <eponde26@gmail.com>
2020-05-15 17:41:24 +0100

    add django_heroku config

--- fondem/settings.py
+++ fondem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p1ispek+h%db-jua=b8t#eg84(l_zifg)g_t25z-xlt!81d9%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fondem.herokuapp.com"]
 
 
 # Application definition
@@ -136,3 +136,4 @@ MEDIAT_ROOT = os.path.join(BASE_DIR, 'media_root')
 SITE_ID = 1
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
